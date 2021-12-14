6e58e9701fdd6e7a5136077747486570dc6aa60b
Vintidois <guimvitor@gmail.com>
2021-12-14 18:07:34 -0300

    create single listing

--- demo.js
+++ demo.js
@@ -8,14 +8,20 @@ async function main () {
     // const uri = 'mongodb+srv://admin:password1234@cluster0.8bj40.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
     const uri = `mongodb+srv://${userdb}:${passdb}@cluster0.8bj40.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
         console.log('connected o mongodb')
 
-        await listDatabases(client);
+        //await listDatabases(client); 
+
+        await createListing(client, { 
+            name: "Lovely Loft",
+            summary: "Loft in Paris",
+            bedrooms: 1,
+            bathrooms: 1
+        })
 
     } catch (err) {
         console.error(err);
@@ -33,4 +39,10 @@ async function listDatabases(client) {
     databasesList.databases.forEach(db => {
         console.log(`- ${db.name}`);
     });
+}
+
+async function createListing(client, newListing) {
+    const result = await client.db('sample_airbnb').collection('listingsAndReviews').insertOne(newListing);
+
+    console.log(`New listing created with id: ${result.insertedId}`);
 }
