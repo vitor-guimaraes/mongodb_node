d758ce0a38a0fb93192c4072c9167e0b565f4a65
Vintidois <guimvitor@gmail.com>
2021-12-16 11:46:43 -0300

    create reservation

--- transaction.js
+++ transaction.js
@@ -29,10 +29,17 @@ async function main() {
 
 
         // Make the appropriate DB calls
-        console.log(
-            createReservationDocument("Infinite Views", [new Date("2019-12-31"), new Date("2020-01-01")],
-            { pricePerNight: 180, specialRequests: "Late checkout", breakfastIncluded: true })
-            );
+        
+        // console.log(
+        //     createReservationDocument("Infinite Views", [new Date("2019-12-31"), new Date("2020-01-01")],
+        //     { pricePerNight: 180, specialRequests: "Late checkout", breakfastIncluded: true })
+        //     );
+
+        await createReservation(client,
+            "leslie@example.com",
+            "Infinite Views",
+            [new Date("2019-12-31"), new Date("2020-01-01")],
+            { pricePerNight: 180, specialRequests: "Late checkout", breakfastIncluded: true });
         
     } finally {
         // Close the connection to the MongoDB cluster
@@ -59,4 +66,61 @@ function createReservationDocument(nameOfListing, reservationDates, reservationD
     }
 
     return reservation;
+}
+
+async function createReservation(client, userEmail, nameOfListing, reservationDates, reservationDetails) {
+    const usersCollection = client.db("sample_airbnb").collection("users");
+    const listingsAndReviewsCollection = client.db("sample_airbnb").collection("listingsAndReviews");
+
+    const reservation = createReservationDocument(nameOfListing, reservationDates, reservationDetails);
+
+    const session = client.startSession();
+
+    const transactionOptions = {
+        readPreference: 'primary',
+        readConcern: { level: 'local' },
+        writeConcern: { w: 'majority' }
+    };
+
+    try {
+        const transactionResults = await session.withTransaction(async () => {
+            
+            const usersUpdateResults = await usersCollection.updateOne(
+                { email: userEmail },
+                { $addToSet: { reservations: reservation } },
+                { session });
+            console.log(`${usersUpdateResults.matchedCount} document(s) found in the users collection with the email address ${userEmail}.`);
+            console.log(`${usersUpdateResults.modifiedCount} document(s) was/were updated to include the reservation.`);
+        }, transactionOptions);
+
+        const isListingReservedResults = await listingsAndReviewsCollection.findOne(
+            { name: nameOfListing, datesReserved: { $in: reservationDates } },
+            { session });
+      if (isListingReservedResults) {
+            await session.abortTransaction();
+            console.error("This listing is already reserved for at least one of the given dates. The reservation could not be created.");
+            console.error("Any operations that already occurred as part of this transaction will be rolled back.");
+            return;
+         }
+
+        const listingsAndReviewsUpdateResults = await listingsAndReviewsCollection.updateOne(
+            { name: nameOfListing },
+            { $addToSet: { datesReserved: { $each: reservationDates } } },
+            { session });
+        console.log(`${listingsAndReviewsUpdateResults.matchedCount} document(s) found in the listingsAndReviews collection with the name ${nameOfListing}.`);
+        console.log(`${listingsAndReviewsUpdateResults.modifiedCount} document(s) was/were updated to include the reservation dates.`);
+
+        if (transactionResults) {
+            console.log("The reservation was successfully created.");
+       } else {
+            console.log("The transaction was intentionally aborted.");
+       }
+       
+    } catch(e){
+        console.log("The transaction was aborted due to an unexpected error: " + e);
+    
+    } finally {
+        await session.endSession();
+    
+    }
 }
